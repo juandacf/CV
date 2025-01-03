a2ddaf02915459204d5da372fa0ad811a88f70a6
Juan Caballero <juandacf@gmail.com>
2025-01-03 18:00:11 -0500

    minor changes added to mobile version

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,9 @@
 var mainButton = document.getElementById("menuMobile")
 var actualMenu =  document.getElementById("pageFeaturesContainer")
+var menuMediaQuery = window.matchMedia('(min-width: 772px)')
+
+
+
 
 mainButton.onclick = function () {
     if(actualMenu.style.display==="flex"){
